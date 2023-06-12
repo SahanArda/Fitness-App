3b0f58759f72b10380111d3faf7a29cdc0c5270f
Arda <ArdaMBP@outlook.com>
2023-06-12 10:32:20 +0100

    fixed problem with .env file

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -2,7 +2,7 @@ export const exercisesOptions = {
     method: 'GET',
     headers: {
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-        'X-RapidAPI-Key': '2ce7f06874msh0cd3ab0601f86dfp12023bjsnf56c27827620' //process.env.REACT_APP_RAPID_API_KEY
+        'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_RAPID_API_KEY
           
     }
 }
